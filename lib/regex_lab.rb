f1d8dd4eb6951ebc1db5e74e2963d6cd8aad4d34
Katleiah Ramos <katleiahramos@gmail.com>
2018-03-14 16:07:04 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -29,7 +29,7 @@ end
 
 def valid_phone_number?(phone)
   #binding.pry
- if phone.match(/.[0-9]{3}+.[0-9]{3}+.[0-9]{3,4}/) == nil
+ if phone.match(/.[0-9]{3}+.[0-9]{3}+.[0-9]{3,4}|[0-9]{10}/) == nil
    binding.pry
    false
  else
