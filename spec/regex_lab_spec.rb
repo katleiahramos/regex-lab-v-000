420209a96c90ccd720a8cf481f1e26d34382989d
Katleiah Ramos <katleiahramos@gmail.com>
2018-03-14 15:56:03 +0000

    automatically backed up by learn

--- spec/regex_lab_spec.rb
+++ spec/regex_lab_spec.rb
@@ -62,17 +62,17 @@ describe "Working with Regular expressions" do
 
   describe "#valid_phone_number?" do
     it "returns true for valid phone numbers, regardless of formatting" do
-      valid_numbers = [ "(718)891-1313", "234 435 9978", "(800)4261134"]
+      valid_numbers = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"]
       expect(valid_numbers.all? { |number| valid_phone_number?(number) }).to be(true)
     end
 
-  #  it "returns false for invalid phone numbers, regardless of formatting" do
-  #    valid_numbers = ["28894546", "(718)891-13135", "234 43 9978", "(800)IloveNY"]
-  #    expect(valid_numbers.all? { |number| valid_phone_number?(number) }).to be(false)
-#    end
+    it "returns false for invalid phone numbers, regardless of formatting" do
+      valid_numbers = ["28894546", "(718)891-13135", "234 43 9978", "(800)IloveNY"]
+      expect(valid_numbers.all? { |number| valid_phone_number?(number) }).to be(false)
+    end
   end
 
 
 end
 
-#"2438894546",
+#,
