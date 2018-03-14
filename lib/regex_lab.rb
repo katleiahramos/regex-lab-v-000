f02444372156705a038046420801c27cd7c616ae
Katleiah Ramos <katleiahramos@gmail.com>
2018-03-14 15:24:31 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,21 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+  if word.capitalize.match(/^[AEIOU]/) == nil
+    false
+  else
+    true
+  end
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
 
+
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+ text.scan(/\b\w{5,5}\b/)
+ binding.pry
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
